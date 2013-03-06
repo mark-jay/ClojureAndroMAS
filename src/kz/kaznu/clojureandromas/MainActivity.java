ed787c243aed638f365dbf2adcf16b49fa8deaba
fallen <fallen.s4e@gmail.com>
2013-03-06 18:14:09 +0600

    init commit

---
+++ src/kz/kaznu/clojureandromas/MainActivity.java
@@ -0,0 +1,38 @@
+package kz.kaznu.clojureandromas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.runButton);
+        
+        /*
+        OnClickListener ocl = new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+			}
+		};
+        button.setOnClickListener(ocl);
+        */
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
